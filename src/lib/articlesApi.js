1960ab5b3c5075c4985542e300920af3434acb62
Randall Nielsen <randallnielsen@Mac.hsd1.nh.comcast.net>
2026-06-26 21:26:44 -0400

    Normalize article text from API

--- src/lib/articlesApi.js
+++ src/lib/articlesApi.js
@@ -1,4 +1,5 @@
 import { DEFAULT_CIVIC_API_BASE, civicApiFetch } from "./civicApi";
+import { cleanText } from "./text";
 
 export function articlesApiBase() {
   return (
@@ -46,5 +47,49 @@ export async function getArticles({
     throw new Error(`Unable to load articles: ${response.status}`);
   }
 
-  return response.json();
+  const data = await response.json();
+
+  return {
+    ...data,
+    articles: (data.articles || data.results || []).map(normalizeArticle),
+  };
+}
+
+function normalizeArticle(article = {}) {
+  return {
+    ...article,
+    title: cleanText(article.title),
+    summary: cleanText(article.summary),
+    publisher: cleanText(article.publisher),
+    resource_type: cleanText(article.resource_type),
+    resourceType: cleanText(article.resourceType),
+    towns: cleanRelationList(article.towns, ["town", "name", "label"]),
+    bills: cleanRelationList(article.bills, ["condensedbillno", "bill_number", "name", "title"]),
+    legislators: cleanRelationList(article.legislators, [
+      "matched_name",
+      "legislator_name_raw",
+      "name",
+      "firstname",
+      "lastname",
+    ]),
+    issueAreas: cleanRelationList(article.issueAreas, ["issue_area", "name"]),
+  };
+}
+
+function cleanRelationList(values, keys = []) {
+  if (!Array.isArray(values)) return values;
+
+  return values.map((value) => {
+    if (typeof value === "string") return cleanText(value);
+    if (!value || typeof value !== "object") return value;
+
+    return {
+      ...value,
+      ...Object.fromEntries(
+        keys
+          .filter((key) => typeof value[key] === "string")
+          .map((key) => [key, cleanText(value[key])]),
+      ),
+    };
+  });
 }
